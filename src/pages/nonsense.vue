f0e3a124670bdfd1aa50674ee78e2ce9eb5522a2
XMZOVO <2980999208@qq.com>
2023-01-15 20:40:15 +0800

    fix: http request to https

--- src/pages/nonsense.vue
+++ src/pages/nonsense.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-  <img src="//36.139.161.136:2052/api/file//%E3%80%8AD%E8%93%9D%E5%AE%9D%E4%B9%A6%E3%80%8B/15%E7%89%88/%E8%93%9D%E5%AE%9D%E4%B9%A615%E7%89%88%20(56).jpg">
+  <img src="https://xmzovo.com:4433/api/file//%E3%80%8AD%E8%93%9D%E5%AE%9D%E4%B9%A6%E3%80%8B/15%E7%89%88/%E8%93%9D%E5%AE%9D%E4%B9%A615%E7%89%88%20(56).jpg">
 </template>
