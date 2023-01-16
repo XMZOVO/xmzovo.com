5130afc47041a896e0ba08966d8639b4a6e83b23
XMZOVO <2980999208@qq.com>
2023-01-16 19:54:02 +0800

    fix: inject order

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
+inject()
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,4 +17,3 @@ const router = createRouter({
 app.use(router)
 app.mount('#app')
 
-inject()
