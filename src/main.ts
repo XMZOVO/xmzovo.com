c2bf049eae04db50791c2b7e62877a328fd7f95c
XMZOVO <2980999208@qq.com>
2023-01-16 20:11:42 +0800

    feat: vitals

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,14 @@ import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+import { webVitals } from './vitals'
 
 inject()
+const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID
+webVitals({
+  path: window.location.pathname,
+  analyticsId,
+})
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
